c527e4f822dcbc8b5afbc989de4cb2386f7a99a2
nagxsan <sanchetng@gmail.com>
2025-03-22 07:33:06 +0530

    feat: implement get_payroll function

--- onchain/contracts/stello_pay_contract/src/payroll.rs
+++ onchain/contracts/stello_pay_contract/src/payroll.rs
@@ -1,4 +1,4 @@
-use soroban_sdk::{contract, contracterror, contractimpl, contracttype, Address, Env};
+use soroban_sdk::{contract, contracterror, contractimpl, contracttype, storage, Address, Env};
 //-----------------------------------------------------------------------------
 // Errors
 //-----------------------------------------------------------------------------
@@ -118,9 +118,12 @@ impl PayrollContract {
 
     /// Gets the payroll details for a given employee.
     /// This can be used by UIs or dashboards to display status.
-    pub fn get_payroll(env: Env, employee: Address) {
+    pub fn get_payroll(env: Env, employee: Address) -> Option<Payroll> {
+        employee.require_auth();
+        let key = PayrollKey(employee.clone());
+        let storage = env.storage().persistent();
 
-        // return -> Option<Payroll>;
+        storage.get::<PayrollKey, Payroll>(&key)
     }
 
     /// Example function to allow employees to pull payment themselves.
