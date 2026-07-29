341e42e2acfae961b7bbde349e94f1ecd6587baf
Lex0865 <destinylawson007@gmail.com>
2026-07-29 22:46:05 +1300

    ci fix

--- onchain/contracts/payment_splitter/tests/test_splitter.rs
+++ onchain/contracts/payment_splitter/tests/test_splitter.rs
@@ -604,7 +604,7 @@ fn test_sum_preservation_property_diverse_splits_and_amounts() {
         vec![10000],                   // 1 recipient, 100%
         vec![5000, 5000],              // 2 recipients, equal
         vec![6000, 4000],              // 2 recipients, unequal
-        vec[]{3333, 3333, 3334},       // 3 recipients, unequal
+        vec![3333, 3333, 3334],       // 3 recipients, unequal
         vec![2500, 2500, 2500, 2500],  // 4 recipients, equal
         vec![1000, 2000, 3000, 4000],  // 4 recipients, unequal
         vec![200; 5],                  // 5 recipients, equal (5*200=1000 ≠ 10000)
